b3ab6f9c8742d10a40916aaa93c45a72052bf2ae
k-monchi <ku.garden12@gmail.com>
2020-12-28 21:36:17 +0900

    20201228 create header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <header>
+      <v-app-bar
+        app
+        light
+      >
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <v-toolbar-title>My-Vue-Blog</v-toolbar-title>
+      </v-app-bar>
+      <!-- ここから -->
+      <v-navigation-drawer
+        v-model="drawer"
+        fixed
+        temporary
+      >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item-group>
+            <v-list-item>
+              <v-list-item-title>HOME</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>ABOUT</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>WORK</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>SERVICE</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>BLOG</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>ACCESS</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+      <!-- ここまで -->
+    </header>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      drawer: false
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
